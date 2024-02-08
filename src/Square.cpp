9289d734cb4aacfca3c042586d6cae9d3e802297
sugarvoid <gamedev@kitargo.com>
2024-02-08 09:15:43 -0600

    add cmake

--- src/Square.cpp
+++ src/Square.cpp
@@ -1,2 +1,49 @@
-#include "Square.h"
+#include "header/Square.h"
+#include "raylib.h"
+#include <iostream>
+
+// Constructor
+Square::Square(int centerX, int centerY, int size) {
+    x = centerX;
+    y = centerY;
+    this->size = size;
+}
+
+// Destructor
+Square::~Square() {
+    // Destructor implementation (if needed)
+}
+
+// Setter functions
+void Square::setCenterX(int centerX) {
+    x = centerX;
+}
+
+void Square::setCenterY(int centerY) {
+    y = centerY;
+}
+
+void Square::setSize(int size) {
+    this->size = size;
+}
+
+// Getter functions
+int Square::getCenterX()  {
+    return x;
+}
+
+int Square::getCenterY()  {
+    return y;
+}
+
+int Square::getSize()  {
+    return size;
+}
+
+// Other member functions
+void Square::draw() const {
+    DrawRectangle(this->getCenterX, this->getCenterX, this->size, this->size, RED);
+   // DrawRectangle(screenWidth/4*2 - 60, 100, 120, 60, RED);
+    std::cout << "Square Center: (" << x << ", " << y << "), Size: " << size << std::endl;
+}
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,43 @@
 #include "raylib.h"
+#include <string>
+#include <iostream>
+#include "Square.h"
+
+
+void makeSquare()
+{
+    // Create a new square object using the constructor
+    Square mySquare(0.0, 0.0, 5.0); // Center at (0,0) and size of 5.0
+
+    
+
+    // Use setter functions to modify the square's properties
+    mySquare.setCenterX(2.0);
+    mySquare.setCenterY(3.0);
+    mySquare.setSize(8.0);
+
+    // Display updated information about the square
+    
+}
+
 
 int main(void)
 {
     InitWindow(800, 800, "raylib [core] example - basic window");
+    SetTargetFPS(60);
+    Square mySquare(40.0, 40.0, 10.0);
+    
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            GetMousePosition();
+            //GetMousePosition();
+            DrawFPS(0,0);
+            mySquare.draw();
             ClearBackground(Color({100, 149, 237, 255}));
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            
+            
         EndDrawing();
     }
 
@@ -17,3 +45,4 @@ int main(void)
 
     return 0;
 }
+
