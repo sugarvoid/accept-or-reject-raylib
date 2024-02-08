9159584ee5e175f0abb6af0f420e7bb5d206c912
sugarvoid <gamedev@kitargo.com>
2024-02-08 10:38:22 -0600

    Upadte gitignore

--- build.sh
+++
@@ -1,4 +0,0 @@
-#g++ main.cpp -Iinclude -Llib -lraylib -lopengl32 -lgdi32 -lwinmm
-
-g++ ./src/*.cpp -o Game -O2 -Wall -Wno-missing-braces -I ./include/ -I ./src/header/ -L ./lib/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 
-./Game
