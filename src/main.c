c3d32ab52c90c43b0c73966b712aaa4e30969367
sugarvoid <gamedev@kitargo.com>
2024-09-22 11:59:24 -0500

    Clean up

--- src/main.c
+++ src/main.c
@@ -1,25 +1,10 @@
 
-//#include "../include/square.hpp"
-
-#include "../include/raylib/raylib.h"
 #include <stdio.h>
 
+#include "../include/raylib/raylib.h"
 #include "../include/square.h"
-
 #include "../include/box2d/box2d.h"
 
-void makeSquare()
-{
-    // // Create a new square object using the constructor
-    // Square mySquare(0.0, 0.0, 5.0); // Center at (0,0) and size of 5.0
-
-    // // Use setter functions to modify the square's properties
-    // mySquare.setCenterX(2.0);
-    // mySquare.setCenterY(3.0);
-    // mySquare.setSize(8.0);
-
-    // // Display updated information about the square
-}
 
 int main(void)
 {
@@ -32,16 +17,12 @@ int main(void)
 
     if (!icon.data)
     {
-        //std::cout << "Failed to load icon" << std::endl;
         printf("Failed to load icon");
     }
 
     SetWindowIcon(icon);
 
-
-
-    //std::cout << "Test Node2d " << player.GetName() << std::endl;
-
+    printf("Ding\n");
  
     Vector2 moPos;
 
@@ -49,6 +30,8 @@ int main(void)
     {
         BeginDrawing();
         moPos = GetMousePosition();
+
+        //printf("%d, %d\n", (int)moPos.x, (int)moPos.y);
         DrawFPS(0, 0);
 
         DrawSquare(&test_s);
